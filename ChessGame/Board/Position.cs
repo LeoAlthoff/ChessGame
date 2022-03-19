eefb2e4598bbb1e4f38554ecb9e73cbcc2844c72
Leonardo Althoff <leonardoalthoff@yahoo.com.br>
2022-03-19 10:29:44 -0300

    Created class Position

---
+++ ChessGame/Board/Position.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Board
+{
+    internal class Position
+    {
+        public int Lines { get; set; }
+        public int Column { get; set; }
+
+        public Position(int lines, int column) { 
+            this.Lines = lines;
+            this.Column = column;
+        }
+
+    }
+}
--- ChessGame/Program.cs
+++ ChessGame/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Board;
+
+namespace ChessGame
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            Position P = new Position(2, 3);
+
+            Console.WriteLine("Position: " + P);
+        }
+    }
+}
